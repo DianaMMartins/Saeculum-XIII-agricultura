023da5f1a8694a1e9a7684d470b7c19317c5aa05
DianaMMartins <dian_marq_mart@hotmail.com>
2023-08-31 21:44:17 +0100

    working on stopping double click on setTimeout

--- src/soilBlock.js
+++ src/soilBlock.js
@@ -13,83 +13,86 @@ export class SoilBlock {
     this.height = 75;
     this.soilClass = ["soil"];
     this.color = "#cd853f";
+    this.isGrowing = false;
 
     this.soil = createEl("button", "soil", x, plant, parentLine);
 
     this.soil.addEventListener("click", () => {
-      if (
-        this.soilAct === soilStages.empty &&
-        this.soil.childElementCount < 1
-      ) {
-        createEl(
-          "div",
-          ["req", "plow-me"],
-          actionTypes.plow,
-          actionTypes.plow,
-          this.soil
-        );
-      }
-      if (
-        this.soilAct === soilStages.empty &&
-        Player.playerAct === soilRequests.plow
-      ) {
-        this.updateSoil(
-          this.soil,
-          soilStages.plowed,
-          "poop-me",
-          actionTypes.poop
-        );
-      } else if (
-        this.soilAct === soilStages.plowed &&
-        Player.playerAct === soilRequests.poop
-      ) {
-        console.log("gets fertilizer");
-
-        this.updateSoil(
-          this.soil,
-          soilStages.pooped,
-          "seed-me",
-          actionTypes.seed
-        );
-      } else if (
-        this.soilAct === soilStages.pooped &&
-        Player.playerAct === soilRequests.seed
-      ) {
-        console.log("gets seed");
-        this.updateSoil(
-          this.soil,
-          soilStages.seeded,
-          "water-me",
-          actionTypes.h2o
-        );
-      } else if (
-        this.soilAct === soilStages.seeded &&
-        Player.playerAct === soilRequests.h2o
-      ) {
-        const plantTime = 2000;
-        const howManyTimesToAskForH2o = 2;
-        this.soil.removeChild(this.soil.lastChild);
-        setTimeout(
-          this.growPlant,
-          plantTime,
-          this.soil,
-          howManyTimesToAskForH2o,
-          soilStages.seedling,
-          actionTypes.h2o
-        );
-        if (counter === howManyTimesToAskForH2o) {
-          this.updateSoilAct(soilStages.seedling);
+      if (!event.detail || event.detail == 1) {
+        if (
+          this.soilAct === soilStages.empty &&
+          this.soil.childElementCount < 1
+        ) {
+          createEl(
+            "div",
+            ["req", "plow-me"],
+            actionTypes.plow,
+            actionTypes.plow,
+            this.soil
+          );
         }
-      } else if (
-        this.soilAct === soilStages.seedling &&
-        Player.playerAct === soilRequests.h2o
-      ) {
-        console.log("next!!!!"); 
-      } else if (
-        this.soilAct === soilStages.ready &&
-        Player.playerAct === soilRequests.pick
-      ) {
-        console.log("last!!!!");
+        if (
+          this.soilAct === soilStages.empty &&
+          Player.playerAct === soilRequests.plow
+        ) {
+          this.updateSoil(
+            this.soil,
+            soilStages.plowed,
+            "poop-me",
+            actionTypes.poop
+          );
+        } else if (
+          this.soilAct === soilStages.plowed &&
+          Player.playerAct === soilRequests.poop
+        ) {
+          console.log("gets fertilizer");
+
+          this.updateSoil(
+            this.soil,
+            soilStages.pooped,
+            "seed-me",
+            actionTypes.seed
+          );
+        } else if (
+          this.soilAct === soilStages.pooped &&
+          Player.playerAct === soilRequests.seed
+        ) {
+          console.log("gets seed");
+          this.updateSoil(
+            this.soil,
+            soilStages.seeded,
+            "water-me",
+            actionTypes.h2o
+          );
+        } else if (
+          this.soilAct === soilStages.seeded &&
+          Player.playerAct === soilRequests.h2o
+        ) {
+          const plantTime = 2000;
+          const howManyTimesToAskForH2o = 2;
+          this.soil.removeChild(this.soil.lastChild);
+          setTimeout(
+            this.growPlant,
+            plantTime,
+            this.soil,
+            howManyTimesToAskForH2o,
+            soilStages.seedling,
+            actionTypes.h2o
+          );
+          if (counter === howManyTimesToAskForH2o) {
+            this.updateSoilAct(soilStages.seedling);
+          }
+        } else if (
+          this.soilAct === soilStages.seedling &&
+          Player.playerAct === soilRequests.h2o
+        ) {
+          console.log("next!!!!");
+        } else if (
+          this.soilAct === soilStages.ready &&
+          Player.playerAct === soilRequests.pick
+        ) {
+          console.log("last!!!!");
+        }
       }
     });
   }
@@ -105,19 +108,23 @@ export class SoilBlock {
   }
 
   growPlant(parent, nRounds, nextSoilStage, nextAction) {
-    if (counter < nRounds) {
-      counter++;
-      createEl(
-        "div",
-        ["req", "water-me"],
-        actionTypes.h2o,
-        actionTypes.h2o,
-        parent
-      );
-    } else {
-      counter = 0;
-      action(parent, nextSoilStage);
-      createEl("div", ["req", "water-me"], nextAction, nextAction, parent);
+    if (!this.isGrowing) {
+      this.isGrowing = true;
+      if (counter < nRounds) {
+        counter++;
+        createEl(
+          "div",
+          ["req", "water-me"],
+          actionTypes.h2o,
+          actionTypes.h2o,
+          parent
+        );
+      } else {
+        counter = 0;
+        action(parent, nextSoilStage);
+        createEl("div", ["req", "water-me"], nextAction, nextAction, parent);
+      }
     }
+    this.isGrowing = false;
   }
 }
