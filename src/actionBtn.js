72587d03d39e9800f1c5809422666c19d1d04438
DianaMMartins <dian_marq_mart@hotmail.com>
2023-09-07 21:06:09 +0100

    cursor changes depending on action selected

--- src/actionBtn.js
+++ src/actionBtn.js
@@ -13,28 +13,23 @@ export class ActionButton {
       switch (this.classList) {
         case "plow-btn":
           Player.playerAct = actionTypes.plow;
-          console.log(Player.playerAct, "player");
           this.updateCursor(actionTypes.plow);
           break;
         case "poop-btn":
           Player.playerAct = actionTypes.poop;
-          console.log(Player.playerAct, "player");
           this.updateCursor(actionTypes.poop);
           break;
         case "seed-btn":
           //player gets to choose a seed type
           Player.playerAct = actionTypes.seed;
-          console.log(Player.playerAct, "player");
           this.updateCursor(actionTypes.seed);
           break;
         case "h2o-btn":
           Player.playerAct = actionTypes.h2o;
-          console.log(Player.playerAct, "player");
           this.updateCursor(actionTypes.h2o);
           break;
         case "pick-btn":
           Player.playerAct = actionTypes.pick;
-          console.log(Player.playerAct, 4);
           this.updateCursor(actionTypes.pick);
           break;
         // Player.cash++; //this can be a whole thing based on plant
@@ -44,8 +39,10 @@ export class ActionButton {
 
   updateCursor(emoji) {
     const parent = document.getElementsByClassName("soil-tbl")[0];
-    const newCursor = "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>" + emoji + "</text></svg>\")' 16 0, auto";
+    const newCursor =
+      `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='68' viewport='0 0 100 100' style='fill:black;font-size:36px;'><text y='50%'>` +
+      emoji +
+      `</text></svg>") 16 0, auto`;
     parent.style.cursor = newCursor;
-    console.log( newCursor, parent.style.cursor);
   }
 }
