5567184f6da7ccea1e53cd1f8651e8f45174a02c
DianaMMartins <dian_marq_mart@hotmail.com>
2023-09-11 09:37:57 +0100

    add way to keep player score

--- src/player.js
+++ src/player.js
@@ -12,4 +12,21 @@ export const player = {
     onionSeeds: 0,
   },
   goal: 100,
+  score: {
+    'cash made': 0,
+    'plants grown': 0,
+    'land': 0,
+    'time spent': 0,
+  },
+  finalScore: function() {
+    const countable = this.score['cash made'] + this.score['plants grown'];
+    const timer = document.getElementById("timer-value").innerText;
+    const timeCalculated = toString(this.score['time spent'] * 60) + timer.slice(1) ;
+    const landCalculated = this.score['land'] * 10000;
+    
+    return scoreboard = {
+      'total' : countable + landCalculated,
+      'time spent': timeCalculated, 
+    };
+  }
 };
